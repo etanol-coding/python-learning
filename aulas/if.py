a2390b058ae1d040bc15a339119a7be1fecde916
etanol-coding <daniel.rizato@gmail.com>
2023-01-18 20:56:08 -0300

    if:.2f

---
+++ aulas/if.py
@@ -0,0 +1,18 @@
+nota = float(input('Quanto você tirou na prova? '))
+karma = 0
+if nota == int(nota):
+    nota = int(nota)
+if nota>10: #nos if sempre utilize :
+    print('O máximo era 10. Deixe de ser mentiroso') 
+elif nota==10:
+    print('O máximo era 10. Você com certeza colou, sua nota era 10. \n M A S  A G O R A  S E R A  Z E R O  S E U  I N C O M P E T E N T E')
+elif nota >= 7:
+    print(f'\nParabéns! a média era 7, Você tirou {nota} e passou!') #identação com TAB para criar um bloco
+    karma == 1
+else:
+    print(f'\n A média era 7, Você tirou {nota}. \n M L K  B U R R O  I N C O M P E T E N T E')
+
+n = 9.83753875873748374
+print('passou de ano' if nota >=7 and nota <10  else f'te vejo ano que vem {n:.2f}')
+
+
--- aulas/tipos-primitivos-e-format.py
+++ aulas/tipos-primitivos-e-format.py
@@ -6,6 +6,7 @@ input('')
 print('tipo primitivo - bool \n true ou false')
 input('')
 print('tipo primitivo - string \n serie de caractéres, sempre em aspas "" \n')
-format = 'format'
-input(f'deseja saber sobre o {format}? \n')
+format = 9.0843593402752678908021094793921508
+input(f'deseja saber sobre o format? {format:.2f}? \n') #coloque um f antes das aspas e usar {} dentro pode colocar variaveis operações etc.
+#usando :.numerof você escolhe quantas casas decimais tera ali
 print('Você sabe demais')
