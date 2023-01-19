eb370152f39f2cffee91ca19cff9d95e6fe1720b
etanol-coding <daniel.rizato@gmail.com>
2023-01-19 00:25:40 -0300

    cores

---
+++ aulas/cores.py
@@ -0,0 +1,15 @@
+# \033[ ; ; m
+# 30       preto      40
+# 31       vermelho   41
+# 32       verde      42
+# 33       amarelo    43
+# 34       azul       44
+# 35       Magenta    45
+# 36       ciano      46
+# 37       cinza      47
+# 97       branco     107
+# 0 - nenhuma
+# 1 - negrito
+# 4 - sublinhado
+# 7 revertido
+print('\033[4;34;43m Olá, mundo!')
