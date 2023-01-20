ffeb3a1b661204498fcb0240abbafd4425670497
etanol-coding <daniel.rizato@gmail.com>
2023-01-20 01:03:54 -0300

    while

--- aulas/laços.py
+++ aulas/laços.py
@@ -1,4 +1,11 @@
 for c in range(1,6): #defina quantas vezes ira se repetir+1
     print(c)
-    if c == 5:
-        print('fim') #coloque ifs dentro de laços!
+    if c == 5:  #coloque ifs dentro de laços!
+        print('fim')
+        c = 0
+
+while c<6:
+    if c==0:
+        c+=1
+    print(c)
+    c+=1
